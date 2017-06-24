8a5605361c371962aa073aa66636c2e51f58d98b
ajlopez <webmaster@ajlopez.com>
2017-06-24 16:25:38 -0300

    Text search text

--- test/textutils.js
+++ test/textutils.js
@@ -24,3 +24,28 @@ exports['normalize options without offset'] = function (test) {
 	test.equal(options.from, 0);
 	test.equal(options.length, 1);
 };
+
+exports['text not found'] = function (test) {
+	var offset = textutils.searchText('foo', 'bar');
+	
+	test.equal(offset, -1);
+};
+
+exports['text not found using length'] = function (test) {
+	var offset = textutils.searchText('foo bar', 'bar', { length: 5 });
+	
+	test.equal(offset, -1);
+};
+
+exports['search text'] = function (test) {
+	var offset = textutils.searchText('foo bar', 'bar');
+	
+	test.equal(offset, 4);
+};
+
+exports['search text using offset'] = function (test) {
+	var offset = textutils.searchText('foo bar foo', 'foo', { from: 4 });
+	
+	test.equal(offset, 8);
+};
+
