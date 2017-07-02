bb590e9aa3ae9146e37e70e4df6bf0ae370f485f
ajlopez <webmaster@ajlopez.com>
2017-07-02 20:02:58 -0300

    Recognizes two professions

--- test/rules/profession.js
+++ test/rules/profession.js
@@ -17,6 +17,16 @@ exports['recognize chemist profession'] = function (test) {
 	test.equal(result, 'profession(harold_urey, chemist)');
 }
 
+exports['recognize two professions'] = function (test) {
+	var result = rule.process({ text: 'she was a naturalized-french physicist and chemist', topic: 'marie_curie' });
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 2);
+	test.equal(result[0], 'profession(marie_curie, physicist)');
+	test.equal(result[1], 'profession(marie_curie, chemist)');
+}
+
 exports['recognize philosopher profession'] = function (test) {
 	var result = rule.process({ text: 'he was a british philosopher', topic: 'david_hume' });
 	
