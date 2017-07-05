d051383281817bc1b699d9c8984ea4d75fdcf3ee
ajlopez <webmaster@ajlopez.com>
2017-07-05 14:32:55 -0300

    Nobel rule with year at end, WIP

--- test/rules/nobel.js
+++ test/rules/nobel.js
@@ -9,4 +9,19 @@ exports['recognize nobel in physics with year'] = function (test) {
 	test.equal(result, 'nobel_prize(richard_feynman, physics, 1965)');
 };
 
+exports['recognize nobel in chemistry with year'] = function (test) {
+	var result = rule.process({ text: '1934 nobel prize in chemistry', topic: 'harold_urey' });
+	
+	test.ok(result);
+	test.equal(typeof result, 'string');
+	test.equal(result, 'nobel_prize(harold_urey, chemistry, 1934)');
+};
+
+exports['recognize nobel in chemistry with year at end'] = function (test) {
+	var result = rule.process({ text: 'nobel prize in chemistry in 1934', topic: 'harold_urey' });
+	
+	test.ok(result);
+	test.equal(typeof result, 'string');
+	test.equal(result, 'nobel_prize(harold_urey, chemistry, 1934)');
+};
 
